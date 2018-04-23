5e3874d21e61d4ab70be64123cbafd54f9da4a0f
chauncyfxm <chauncyfxm@aliyun.com>
2018-04-23 14:24:16 +0800

    j任务
    任务

--- HTML_01/honghaizi/js/js_01.js
+++ HTML_01/honghaizi/js/js_01.js
@@ -1,24 +1,103 @@
-// alert("我是外部的你好");
-window.onload = function(){
-	function my_display(trigger,effect){
-	var oDisplay = document.getElementById(trigger);
-	var oToDisplay = document.getElementById(effect);
-	// alert("你好啊！");
-	// alert(oToDisplay.innerHTML);
+// // 根据类获取对象列表
+// function getElementByClassName(classname){
+//     var tags=document.getElementsByTagName("*");
+//     var list=[];
+//     for(var i in tags){
+//         var tag=tags[i];
+//         if(tag.className==classname){list.push(tag);}
+//     }
+//     return list;
+// }
 
-	oDisplay.onmouseover = function(){
-	// alert("你好啊_02！");
-		oToDisplay.style.display = "block";
+// // *************************
 
-		// if(oToDisplay.style.display == "none"){
-		// 	oToDisplay.style.display = "block";
-		// }else{
-		// 	oToDisplay.style.display = "none";
-		// }
-	}
 
-	oDisplay.onmouseout = function(){
-		oToDisplay.style.display = "none";
-	}
-	}
+
+// function my_display(oDisplay,effect){
+// 	var oToDisplay = document.getElementById(effect);
+// 		oToDisplay.style.display = "block";
+
+// 	oDisplay.onmouseout = function(){
+// 		oToDisplay.style.display = "none";
+// 	}
+// }
+
+function my_display_01(class_01,class_02){
+	var list_01 = document.getElementsByClassName(class_01);
+	var list_02 = document.getElementsByClassName(class_02);
+	for (var i = list_01.length - 1; i >= 0; i--) {
+		list_01[i].style.background = "#fff";
+		list_01[i].style.color = "#000";
+		}
+
+	for (var i = list_02.length - 1; i >= 0; i--) {
+		list_02[i].style.display = "block";
+		}
+
+
 }
+
+function my_display_02(class_01,class_02){
+	var list_01 = document.getElementsByClassName(class_01);
+	var list_02 = document.getElementsByClassName(class_02);
+	for (var i = list_01.length - 1; i >= 0; i--) {
+		list_01[i].style.background = "#000";
+		list_01[i].style.color = "#fff";
+	}
+
+	for (var i = list_02.length - 1; i >= 0; i--) {
+		list_02[i].style.display = "none";
+	}
+
+}
+
+
+
+
+
+// window.onload = function(){
+
+// 	var x = document.getElementsByClassName("clearfix");
+
+// 	console.log(x);
+// // function getElementByClassName(classname){
+// //     var tags=document.getElementsByTagName("*");
+// //     var list=[];
+// //     for(var i in tags){
+// //     	console.log("我是全部类:" + tags[i].className);
+// //         var tag=tags[i];
+// //         var string = tag.className + "1";
+// //         console.log("我是string:"+string);
+// //         if(string.indexOf(classname)!=(-1)){list.push(tag);}
+// //     }
+// //     for(var i=0; i<list.length; i++){
+// //     	console.log(i);
+// //     	console.log("我是列表：" + list[i].className);
+// //     }
+// //     return list;
+
+// // }
+// // getElementByClassName("clearfix");
+
+// }
+
+
+
+// document.getElementsByClassName=function(className){  
+//     var children = document.getElementsByTagName('*') || document.all;
+//     var elements=new Array();
+//     for(var i=0;i<children.length;i++)  
+//     {  
+//         var child=children[i];  
+//         var classNames=child.className.split(' ');  
+//         for(var j=0;j<classNames.length;j++)  
+//         {  
+//             if(className==classNames[j])  
+//             {  
+//                 elements.push(child);  
+//                 break;  
+//             }  
+//         }  
+//     }  
+//     return elements;  
+// }
